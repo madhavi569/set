4711cc4a4b53130f88d7524c647fe890f61e17e4
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 10:40:58 +0530

    Create Space.py

---
+++ Space.py
@@ -0,0 +1,6 @@
+'''
+Write a program to get the set values in a single line separated by space (including duplicate values) and print the number of elements in the given set.
+Sample Input:
+1 2 3 4 5 1 2 3
+'''
+5
