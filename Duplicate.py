689dd6aa7c89c2036dcd6e0876905e30c575fa43
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 10:44:03 +0530

    Create Duplicate.py

---
+++ Duplicate.py
@@ -0,0 +1,13 @@
+'''
+ Write a program to get set values in a single line with space(including duplicate values) and find the number of duplicate values given during input and print the output set value without duplicate elements.
+Sample Input:
+6
+1
+2
+1
+2
+3
+1
+'''
+Duplicate Values: 3
+1 2 3 
